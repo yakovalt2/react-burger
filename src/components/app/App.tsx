a52f140a14a452cc3df12df1db08223f262171f1
yakovalt2 <yakovalt2@gmail.com>
2025-10-18 22:01:13 +0300

    Добавлен запрос к API

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,16 +2,51 @@ import React from 'react';
 import AppHeader from '../app-header/AppHeader';
 import styles from './App.module.css';
 import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
-import { ingredientsData } from '../../utils/data';
+// import { ingredientsData } from '../../utils/data';
 import BurgerConstructor from '../burger-constructor/BurgerConstructor';
+import { useState, useEffect } from 'react';
+import { TIngredient } from '../../utils/types';
+import { API_URL } from '../../utils/constants';
 
-const App = () => {
+function App() {
+  const [ingredients, setIngredients] = useState<TIngredient[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const res = await fetch(`${API_URL}/ingredients`);
+         if (!res.ok) {
+          throw new Error(`Ошибка сервера: ${res.status}`);
+        }
+        const data = await res.json();
+        setIngredients(data.data);
+      } catch (error) {
+        setHasError(true);
+        console.error('Ошибка при загрузке ингредиентов:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchIngredients();
+  }, []);
+
+  if (isLoading) {
+    return <p>Загрузка...</p>;
+  }
+
+  if (hasError) {
+    return <p>Произошла ошибка при загрузке данных 😢</p>;
+  }
+
   return (
     <div className={styles.app}>
       <AppHeader />
       <main className={styles.main}>
-        <BurgerIngredients ingredients={ingredientsData} />
-        <BurgerConstructor ingredients={ingredientsData} />
+        <BurgerIngredients ingredients={ingredients} />
+        <BurgerConstructor ingredients={ingredients} />
       </main>
     </div>
   );
