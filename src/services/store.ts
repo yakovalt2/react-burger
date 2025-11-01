cdaa51b4316199af24099b442dbcc672de1aab8f
yakovalt2 <yakovalt2@gmail.com>
2025-11-01 12:23:00 +0300

    Добавлен Redux DevTools

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,11 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ingredientsReducer from './slices/IngredientsSlice';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./rootReducer";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
-  reducer: {
-    ingredients: ingredientsReducer,
-  },
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production", 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
